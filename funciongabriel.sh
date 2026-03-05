e2de1924a76d7064e24ba91fabe6f7150ba5d11b
Gabriel <gabrielbusiness2007@gmail.com>
2026-03-05 13:05:28 +0100

    prueba funcion

---
+++ funciongabriel.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#Titulo: Funcion 
+#Autor: Gabriel Merencio Ortega
+#Fecha: 
+#Descripción:
+
+# ====== Colores ======
+RESET=$'\033[0m'
+RED=$'\033[31m'      # error
+GREEN=$'\033[32m'    # perfecto
+YELLOW=$'\033[33m'   # señalar
