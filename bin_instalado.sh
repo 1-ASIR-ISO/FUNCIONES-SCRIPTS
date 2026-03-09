c23aa09b120f53e726ab652cd7880b340e5a32d5
Adriandelpar <adriandelpar@gmail.com>
2026-03-09 14:15:43 +0100

    Scrip bin_instaldo subido

---
+++ bin_instalado.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Autor: Adrián
+# Descripción: Va a comprobar si tienes un binario instalado y devolvera un 0 si sí y un 1 si no.
+# Fecha: 09/03/2026
+
+error="\e[31m"
+azul="\e[1;34m"
+exito="\e[32m"
+reset="\e[0m"
+
+# ========================================= bin_instalado
+
+function f_bin_instalado {
+
+}
