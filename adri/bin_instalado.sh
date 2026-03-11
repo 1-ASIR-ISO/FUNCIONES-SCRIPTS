36a0650d2802f9b16710c86f2d45958bef23728f
Adriandelpar <adriandelpar@gmail.com>
2026-03-11 14:15:38 +0100

    Actualizado funciones adrian

--- adri/bin_instalado.sh
+++ adri/bin_instalado.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 # Autor: Adrián
-# Descripción: Va a comprobar si tienes un binario instalado y devolvera un 0 si sí y un 1 si no.
 # Fecha: 09/03/2026
 
 error="\e[31m"
@@ -10,19 +9,21 @@ reset="\e[0m"
 
 # ========================================= bin_instalado
 
-echo -e "${azul}Dime el nombre del binario${reset}"
-read bin
+# Nombre: f_bin_instalado
+# Descripción: Va a comprobar si tienes un binario instalado y devolvera un 0 s>
+# Parametros Entrada: 1
+# Parametros Salida: Devuelve si esta o no esta instalado el bin
 
 f_bin_instalado(){
-  if command -v $bin &>/dev/null; then
-	echo -e "${exito}Si está instalado ${reset}"
+  if command -v "$1" &>/dev/null; then
+	printf "${exito}Si está instalado ${reset}\n"
 	return 0
 
   else
-	echo -e "${error}No está instalado ${reset}"
+	printf "${error}No está instalado ${reset}\n"
 	return 1
 
   fi
 }
 
-f_bin_instalado $bin
+f_bin_instalado "$1"
--- adri/hay_conexion.sh
+++ adri/hay_conexion.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 # Autor: Adrián
-# Descripción: Comprueba si hay conexion o no
 # Fecha: 10/03/26
 
 error="\e[31m"
@@ -8,14 +7,19 @@ azul="\e[1;34m"
 exito="\e[32m"
 reset="\e[0m"
 
-# ========================================= hay_conexion
+# =========================================
+
+# Nombre: f_hay_conexion
+# Descripción: Comprueba si hay conexion o no en la maquina.
+# Parametros Entrada: 0
+# Parametros Salida: Muestra si hay o no conexión.
 
 f_hay_conexion(){
 	if ping -c 1 -q 8.8.8.8 > /dev/null 2>&1;then
-		echo -e "${exito}Si hay conexión${reset}"
+		printf "${exito}Si hay conexión${reset}"
 		return 0
 	else
-		echo -e "${error}No hay conexión${reset}"
+		printf "${error}No hay conexión${reset}"
 		return 1
 	fi
 }
