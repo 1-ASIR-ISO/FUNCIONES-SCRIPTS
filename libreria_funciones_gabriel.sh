f75c0daa97d7369a4e947bb921ac2c235b27a9ea
Gabriel <gabrielbusiness2007@gmail.com>
2026-03-11 12:59:54 +0100

    Modificado

--- libreria_funciones_gabriel.sh
+++ libreria_funciones_gabriel.sh
@@ -3,48 +3,55 @@
 # Descripción: funcion dedicada a buscar paquetes en el sistema a traves del binario que pasa el usuario como argumento
 # Fecha: 09-3-2026
 
-error=$'\e[31m'      
-azul=$'\e[1;34m'
-exito=$'\e[32m'
-reset=$'\e[0m'
+error=$'\e[31m'       # Rojo
+azul=$'\e[1;34m'      # Azul intenso
+exito=$'\e[32m'       # Verde
+reset=$'\e[0m'        # Reset
 
 # =========================================
+
 f_uid() {
     id -u
 }
 
 f_buscar_paquetes() {
-    echo -n "Introduce la ruta al binario: "
+    echo -n "${azul}Introduce la ruta al binario: ${reset}"
     read binario
-if [ -z "$binario" ]; then
-        echo "❌ Debes introducir un binario$ correcto ❌" >&2
+    if [ -z "$binario" ]; then
+        echo "${error}Debes introducir un binario correcto ❌${reset}" >&2
         return 1
     fi
     paquete=$(dpkg -S "$binario" 2>/dev/null | head -n1 | cut -d: -f1)
     if [ -n "$paquete" ]; then
-        echo "✅ Paquete: $paquete ✅"
+        echo "${exito}Paquete: $paquete ✅${reset}"
         return 0
     else
-        echo "❌ Paquete no encontrado para '$binario'❌"
+        echo "${error}Paquete no encontrado para '$binario' ❌${reset}"
         return 1 
     fi
 }
 
 f_paquete_instalado() {
-    echo -n "Introduce nombre del paquete: "
+    echo -n "${azul}Introduce nombre del paquete: ${reset}"
     read paquete
     if [ -z "$paquete" ]; then
-        echo "❌ Debes introducir un paquete valido" >&2
+        echo "${error}Debes introducir un paquete valido${reset}" >&2
         return 1
     fi
     if dpkg-query -W -f='${Status}' "$paquete" 2>/dev/null | grep -q "ok installed"; then
-        echo "✅ $paquete ESTÁ INSTALADO"
+        echo "${exito}$paquete ESTÁ INSTALADO${reset}"
         return 0
     else
-        echo "❌ $paquete NO está instalado"
+        echo "${error}$paquete NO está instalado${reset}"
         return 1
     fi
 }
+
+
+
+
+
+
 
 # =========================================
 f_listar_interfaces() {
