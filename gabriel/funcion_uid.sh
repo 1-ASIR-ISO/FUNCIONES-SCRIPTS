a15c95679e0deb153a508542d96a5ebfef185293
Gabriel <gabrielbusiness2007@gmail.com>
2026-03-12 10:03:46 +0100

    Finished

--- gabriel/funcion_uid.sh
+++ gabriel/funcion_uid.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 # Autor: Gabriel
-# Descripción:
+# Descripción: Obtiene UID del usuario actual
 
-error="\e[31m"
-azul="\e[1;34m"
-exito="\e[32m"
-reset="\e[0m"
+error="\\e[31m"
+azul="\\e[1;34m"
+exito="\\e[32m"
+reset="\\e[0m"
 
 # =========================================
-
-#Nombre: f_uid
-#Descripción: función para saber el UID del usuario que ejecuta el script.
-#Entrada: No.
-#Salida: Parametro UID del usuario.
+# Nombre: f_uid
+# Descripción: función para saber el UID del usuario que ejecuta el script.
+# Entrada: 0.
+# Salida: Muestra UID del usuario con color.
 
 f_uid() {
-    id -u
+    uid=$(id -u)
+    printf "${exito}La UID del usuario es: %s${reset}\\n" "$uid"
 }
+
+f_uid
